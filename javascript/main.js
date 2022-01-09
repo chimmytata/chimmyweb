ffbacccc42a1bb0e98809df918363bca9db05c21
chimmytata <yt944223@gmail.com>
2022-01-09 13:52:22 +0800

    20220109 多條件判斷式 及 全域變數與區域變數判別

--- javascript/main.js
+++ javascript/main.js
@@ -2,30 +2,29 @@
 /* 雙引號（"）與 單引號（'） 為字串 都可以使用 字串不是數字 無法加減
    但習慣是 雙引號：自己打的  單引號：javascrip原使用*/
 
-TestCode();
+// TestCode();
 
-// 日期函數 Date 產新的 Date
-var year = new Date().getFullYear();
-// var now = new Date;
-// var year = now.getFullYear();
+TimeYear();
 
-// 年份變數
-// var Year = "2022";
-// 由 html 顯示資料
-document.getElementById('Year').textContent = year;
+function TimeYear(){
+    // 日期函數 Date 產新的 Date
+    var year = new Date().getFullYear();
+    // var now = new Date;
+    // var year = now.getFullYear();
 
-
+    // 年份變數
+    // var Year = "2022";
+    // 由 html 顯示資料
+    document.getElementById('Year').textContent = year;
+}
 
 // 設計 方法 function name(參數或空白){程式}
 function TestCode(){
     // 變數 命名：英文開頭（不可數字開頭），英文與數字，底線，javascrip關鍵字不可能設定，英文可設大小寫混用
-// es5版本 var
-// 宣告變數 全域變數
+    // es5版本 var；es6版本 let（變數） const（常數）
+    // 宣告變數 全域變數
     var one;
     var ONE;
-    var One;
-    var O_ne;
-    var O_01ne;
 
     // 賦予 值
     one = 1;
@@ -59,4 +58,32 @@ function TestCode(){
         console.log("NumBer < Num");
     else 
         console.log("NumBer < Num 不成立");
+
+    // 多條件判斷式 if(條件){程式} else if(條件二) {程式} else{程式}
+    // 全域變數
+    let Num_One = 1;
+    let Num_Two = 2;
+    // == 等於（比較）
+    if ( Num_One == Num_Two ) {
+        console.log("兩個變數相等")
+    } else if ( Num_Two > Num_One ) {
+        console.log(Num_Two + " 大於 " + Num_One);
+    } else {
+        console.log("不成立")
+    };
 }
+
+// 全域變數
+var a = 1;
+let b = 2;
+
+// var 與 let 的差別
+function Var_LetCode() {
+    // 區域變數 
+    var a = 3;
+    let b = 4;
+
+    console.log( a + b );
+}
+    
+Var_LetCode();
