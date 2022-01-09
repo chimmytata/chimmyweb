5f8baac2afab05dd801a7681885d05dadd14bd02
chimmytata <yt944223@gmail.com>
2022-01-09 12:08:07 +0800

    20220109 funtion

--- javascript/main.js
+++ javascript/main.js
@@ -2,26 +2,10 @@
 /* 雙引號（"）與 單引號（'） 為字串 都可以使用 字串不是數字 無法加減
    但習慣是 雙引號：自己打的  單引號：javascrip原使用*/
 
-// 變數 命名：英文開頭（不可數字開頭），英文與數字，底線，javascrip關鍵字不可能設定，英文可設大小寫混用
-// es5版本 var
-// 宣告變數 全域變數
-var one;
-var ONE;
-var One;
-var O_ne;
-var O_01ne;
-
-// 賦予 值
-one = 1;
-ONE = "01";
-
-console.log(one);
-console.log(ONE);
-console.log('02');
+TestCode();
 
 // 日期函數 Date 產新的 Date
 var year = new Date().getFullYear();
-
 // var now = new Date;
 // var year = now.getFullYear();
 
@@ -30,21 +14,49 @@ var year = new Date().getFullYear();
 // 由 html 顯示資料
 document.getElementById('Year').textContent = year;
 
-// 條件判斷 if.....else 單一條件
-var NumBer = 12;
-// 顯示條件成立，執行的資料
-if (NumBer < 5) {
-    console.log("數字小於 5");
+
+
+// 設計 方法 function name(參數或空白){程式}
+function TestCode(){
+    // 變數 命名：英文開頭（不可數字開頭），英文與數字，底線，javascrip關鍵字不可能設定，英文可設大小寫混用
+// es5版本 var
+// 宣告變數 全域變數
+    var one;
+    var ONE;
+    var One;
+    var O_ne;
+    var O_01ne;
+
+    // 賦予 值
+    one = 1;
+    ONE = "01";
+
+    console.log(one);
+    console.log(ONE);
+    console.log('02');
+
+    // 條件判斷 if.....else 單一條件
+    var NumBer = 12;
+    // 顯示條件成立，執行的資料
+    if (NumBer < 5) {
+        console.log("數字小於 5");
+    }
+
+    if (NumBer > 10)console.log("數字大於 10");
+
+    if (NumBer > 10)
+        console.log("數字為：" + NumBer);
+
+    // 顯示條件成立，執行的資料；或是不成立時，執行的資料
+    var Num = 20;
+    if ( NumBer < Num) {
+        console.log("NumBer < Num")
+    } else {
+        console.log("NumBer < Num 不成立")
+    }
+
+    if ( NumBer < Num) 
+        console.log("NumBer < Num");
+    else 
+        console.log("NumBer < Num 不成立");
 }
-
-if (NumBer > 10)console.log("數字大於 10");
-
-if (NumBer > 10)
-    console.log("數字為：" + NumBer);
-
-// 顯示條件成立，執行的資料；或是不成立時，執行的資料
-// if () {
-
-// } else {
-    
-// }
