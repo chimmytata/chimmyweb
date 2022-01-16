4832a3b90c7db219695692909fe33ccf2e7c8409
chimmytata <yt944223@gmail.com>
2022-01-16 15:00:01 +0800

    20220116 手機板呈現的SmallNav，用javascript撰寫

--- javascript/main.js
+++ javascript/main.js
@@ -26,7 +26,49 @@ function CopyRightContent () {
     // 調整 框架 位置
     // 呼叫要選取的標籤
     let Address = document.getElementById('Address');
-    
+
     // 3. 在被選取的元素之前插入內容 before() 置入被選取元素標籤之前
     Address.before(div);
+}
+
+SmallNavCon ();
+// 新增 SmallNav 的內容
+function SmallNavCon () {
+    // 新增標籤 ul 與 li
+    let ul = document.createElement('ul');
+    
+
+    // 在 SmallNav 內新增，宣告變數
+    let SN = document.getElementById('SmallNav');
+
+    // 建立 ul 框架，給予 id 名稱
+    SN.appendChild(ul);
+
+    // 賦予屬性 id or class
+    ul.setAttribute("id","PhoneNav");
+
+    // 陣列
+    let PhoneNav = document.getElementById('PhoneNav');
+    let NavClass = [ 'About', 'VerticalLine', 'Shop', 'VerticalLine', 'Message', 'VerticalLine', 'Address'];
+    let NavCon = [ '品牌故事', '|', '購物說明', '|', '客服留言', '|', '門市資訊']
+
+    // 新增標籤時，程式的運作方式為：先新增標籤，賦予屬性，在指定新增的外框架（此為一個迴圈）
+    // 迴圈內的數字，是因為陣列數而產生，故要隨著陣列的數量而自動增減
+    for ( let i = 0; i < NavClass.length; i++) {
+        let li = document.createElement('li');
+        // 新增屬性
+        li.setAttribute("class", NavClass[i]);
+        // ul 內新增 li 清單
+        PhoneNav.appendChild(li);
+
+        // 新增連結 a
+        let A = document.createElement('a');
+        // 新增屬性 假連結 javascript:;
+        A.setAttribute("href", "javascript:;");
+        // li 內新增 a 連結
+        li.appendChild(A);
+        // 置入 陣列內容
+        A.innerText = NavCon[i];
+    }
+    
 }
