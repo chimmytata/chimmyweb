2e4ab1cf6982aefebe276bbba050bf84fd61444b
chimmytata <yt944223@gmail.com>
2022-02-19 13:36:28 +0800

    20220213 修正 load 路徑

--- javascript/main.js
+++ javascript/main.js
@@ -88,7 +88,7 @@ function PhoneMenushow () {
     Menu.style.display = "block";
 }
 
-
+// 修正load路徑 原本為 ../web/header.html 在本機中沒有問題，但是到網路會有問題，所以將 ../拿掉
 Pageload();
 function Pageload() {
     $('header').load("web/header.html");
