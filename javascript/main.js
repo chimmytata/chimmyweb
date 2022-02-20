f3fb9ad15f6f8ffce7b2feb404c96749baed9d7a
chimmytata <yt944223@gmail.com>
2022-02-20 13:48:33 +0800

    20220219 上午上課

--- javascript/main.js
+++ javascript/main.js
@@ -101,3 +101,97 @@ function SmallNavCon () {
     }
     
 }
+
+NewCommodityCon();
+// 新增 新品上市 的內容
+function NewCommodityCon() {
+
+    // 新增標籤 section
+    let sec = document.createElement('section');
+
+    // 在 NewCommodityCon 內新增 section
+    let SE = document.getElementById('NewCommodityCon');
+
+    // 建立 section 框架，給予名稱
+    SE.appendChild(sec);
+
+    // 給予 class 和 id 名稱
+    sec.setAttribute("id","NCommodity");
+    sec.setAttribute("class","NCommodity"); 
+
+    /* ============================================================== */
+    // 新增標籤 ul
+    let ul = document.createElement('ul');
+
+    // 在 NCommodity 內新增 ul
+    let CO = document.getElementById('NCommodity');
+
+    // 建立 ul 框架
+    CO.appendChild(ul);
+
+    // 給予 id 名稱
+    ul.setAttribute("id","TitleBox");
+
+    // 設計迴圈
+    // li 在 TitleBox 框架內產生，宣告 TitleBox 的變數
+    let TB = document.getElementById('TitleBox');
+
+    // 宣告 li 的 class 名稱 array（陣列）
+    let LiClass = ['Title','Number','Text'];
+
+    // 宣告 li 的內容
+    let LiCon = ['新品上市','庫存','數量有限，售完為止'];
+
+    // for 迴圈
+    for(let i = 0;i<LiClass.length;i++) {
+        // 建立 li
+        let li = document.createElement('li');
+
+        // 在 TitleBox （宣告在 137 行 TB）
+        TB.appendChild(li);
+
+        // 新增 class 屬性
+        li.setAttribute("class",LiClass[i]);
+
+        // 置入 內容
+        li.innerText = LiCon[i];
+    }
+
+    // 建立 class 的變數
+    // let Number = TB.getElementsByClassName('Number')[0];
+    // querySelector() 在文件中匹配指定 css 選擇器
+    // let Number = document.querySelector('.Number');
+    let Number = TB.querySelector('.Number');
+
+    // 建立 span 標籤
+    let SP = document.createElement('span');
+
+    // 在 Number 建立 span
+    Number.appendChild(SP);
+
+    SP.innerText = '1';
+    /* ============================================================== */
+
+    // 設定 div class 名稱陣列
+    let DClass = ['Images','LName','Brief'];
+
+    for(let i =0;i<DClass.length;i++) {
+        // 新增 div 的框架，宣告變數
+        let Div = document.createElement('div');
+
+        // 在 Commodity
+        CO.appendChild(Div);
+
+        // 新增屬性
+        Div.setAttribute("class",DClass[i]);
+    }
+
+    // Images 內新增資料
+    let A = document.createElement('a');
+    // 新增 連結 屬性
+    A.setAttribute("href","javascript:;");
+    // 宣告 指定 class 的變數
+    let Images = CO.querySelector('.Images');
+    // 在 Images 內新增
+    Images.appendChild(A);
+}
