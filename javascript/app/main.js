41dbff658455cc6133d25b2dd643b207174382eb
chimmytata <yt944223@gmail.com>
2022-03-20 16:48:03 +0800

    20220320 下午上課

--- javascript/app/main.js
+++ javascript/app/main.js
@@ -1,5 +1,6 @@
 // 只要將檔名寫入陣列中（不用給附檔名），即可直接讀取檔案內容
-let IndexCon =["jquery", "jquery.menunav", "copyright", "SmallNav", "merchandise"];
+let IndexCon = ["jquery", "jquery.menunav", "copyright", "SmallNav", "NewCommodity", "Share", "Banner"];
+let FooterCon = ["copyright"];
 
 define(IndexCon, function($) {
     
@@ -7,5 +8,7 @@ define(IndexCon, function($) {
     $(function() {
         PhoneMenushow2 ();
         CopyRightContent ();
+        NewCommodityCon(8);
+        ShareCon(8);
     });
 });
